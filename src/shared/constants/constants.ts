fcf90902a5ba4019ad1e09c0523cdbbbc7e2181c
alexandre-silvaa <alexandrebsilva9@gmail.com>
2025-03-12 22:22:36 -0300

    refactor: 🎨 Database Env

--- src/shared/constants/constants.ts
+++ src/shared/constants/constants.ts
@@ -1,7 +1,3 @@
 import 'dotenv/config';
 
-export const DB_HOST = process.env.DB_HOST;
-export const DB_PORT = process.env.DB_PORT;
-export const DB_DATABASE = process.env.DB_DATABASE;
-export const DB_USERNAME = process.env.DB_USERNAME;
-export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_URL = process.env.DATABASE_URL;
